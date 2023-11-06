4b9f12380973d77c3cb945f5b4577c052b4fc718
Raian010 <md.abrarulhoque123@gmail.com>
2023-11-06 11:26:01 +0600

    reviwe section

--- src/Pages/Home/CategoryTabs.jsx
+++ src/Pages/Home/CategoryTabs.jsx
@@ -1,23 +1,48 @@
-// import { useEffect } from "react";
-// import { useState } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 
 const CategoryTabs = () => {
 
-    // const [categories,setCategories] = useState([]);
-    // useEffect(() => {
-    //     fetch("http://localhost:5000/jobs")
-    //     .then(res => res.json())
-    //     .then(data => setCategories(data))
-    // },[])
+    const [selectCategory,setSelectCategory] = useState("Web Development");
+    const [categories,setCategories] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/jobs")
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    },[])
 
     return (
         <div>
-            <h2>This is tabs page </h2>
+            <h2 className="text-4xl text-center font-bold mb-10">Our <span className="text-blue-600">Category</span></h2>
             <div className="tabs">
-  <a className="tab tab-lifted">Tab 1</a> 
-  <a className="tab tab-lifted tab-active">Tab 2</a> 
-  <a className="tab tab-lifted">Tab 3</a>
+                {["Web Development", "Digital Marketing", "Graphic Design"].map(category => 
+                <a
+                key={category} className={`tab tab-lifted ${selectCategory === category ? 'tab-active' : ''} text-xl font-semibold`}
+                
+                onClick={() => setSelectCategory(category)}>
+                      {category}
+                </a>)}
+
+</div>
+
+<div className="category-content mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+    {categories.map(item => {
+        if(selectCategory === item.category){
+            return <div key={item._id} className="card bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title h-[52px] text-xl font-bold">{item.job}</h2>
+              <p className="font-medium">Deadline: <span className="ml-2">{item.deadline}</span></p> 
+              <p className="font-medium">Price range: <span className="ml-2">{item.minimum}$-{item.maximum}$</span></p> 
+              <p className="font-medium h-[192px]">{item.description}</p> 
+             
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Bid Now</button>
+              </div>
+            </div>
+          </div>
+        }
+    })}
 </div>
         </div>
     );
